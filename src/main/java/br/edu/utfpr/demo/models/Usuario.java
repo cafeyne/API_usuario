b9802c6766636e62d0fdc10d55cf4884f2fdb5a2
Mayne Fogassa <114357908+cafeyne@users.noreply.github.com>
2024-11-18 13:45:19 -0300

    Update Usuario.java

--- src/main/java/br/edu/utfpr/demo/models/Usuario.java
+++ src/main/java/br/edu/utfpr/demo/models/Usuario.java
@@ -22,7 +22,7 @@ import java.time.LocalDateTime;
 public class Usuario {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     @Column(nullable = false, length = 100)
